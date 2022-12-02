829816bcf3e9abdd71c7f42756c4291733a6d975
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-12-02 15:07:38 +0000

    Add comment

--- src/hosting/GenericWebHostService.ts
+++ src/hosting/GenericWebHostService.ts
@@ -1,6 +1,7 @@
 import { IHostedService } from '@/hosting/IHostedService';
 import { injectable } from 'inversify';
 
+// https://github.com/dotnet/aspnetcore/blob/600eb9aa53c052ec7327e2399744215dbe493a89/src/Hosting/Hosting/src/GenericHost/GenericWebHostService.cs#L17
 @injectable()
 export class GenericWebHostService implements IHostedService {
 	start = async (): Promise<void> => {
