4b203a09550c0d43f9d6f17525080291478a4140
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-12-08 06:41:44 +0000

    Return NotFoundFileInfo

--- src/fileProviders/PhysicalFileProvider.ts
+++ src/fileProviders/PhysicalFileProvider.ts
@@ -1,6 +1,7 @@
 import { IDisposable } from '@/base/IDisposable';
 import { IFileInfo } from '@/fileProviders/IFileInfo';
 import { IFileProvider } from '@/fileProviders/IFileProvider';
+import { NotFoundFileInfo } from '@/fileProviders/NotFoundFileInfo';
 
 // https://source.dot.net/#Microsoft.Extensions.FileProviders.Physical/PhysicalFileProvider.cs,deeb5176dbadb21d,references
 export class PhysicalFileProvider implements IFileProvider, IDisposable {
@@ -11,6 +12,7 @@ export class PhysicalFileProvider implements IFileProvider, IDisposable {
 	};
 
 	getFileInfo = (subpath: string): IFileInfo => {
-		throw new Error('Method not implemented.');
+		// TODO
+		return new NotFoundFileInfo(subpath);
 	};
 }
