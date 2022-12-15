9d4ea8c128d35ab37ed6891585a0710437006332
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-12-15 11:04:38 +0000

    Remove inversify.config

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -2,4 +2,3 @@ export { AppBuilder } from './AppBuilder';
 export { HttpContext } from './HttpContext';
 export { HttpRequest } from './HttpRequest';
 export { HttpResponse } from './HttpResponse';
-export { container } from './inversify.config';
