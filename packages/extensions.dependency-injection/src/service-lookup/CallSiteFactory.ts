733533c0d20fa51ede85630b9b60f0a5672d3666
Aigamo <ycanardeau@gmail.com>
2022-12-22 08:17:33 +0000

    Create ServiceCacheKey

--- packages/extensions.dependency-injection/src/service-lookup/CallSiteFactory.ts
+++ packages/extensions.dependency-injection/src/service-lookup/CallSiteFactory.ts
@@ -3,18 +3,81 @@ import { Type } from '@yohira/base/Type';
 import { IServiceProviderIsService } from '@yohira/extensions.dependency-injection.abstractions/IServiceProviderIsService';
 import { ServiceDescriptor } from '@yohira/extensions.dependency-injection.abstractions/ServiceDescriptor';
 import { CallSiteChain } from '@yohira/extensions.dependency-injection/service-lookup/CallSiteChain';
+import { ServiceCacheKey } from '@yohira/extensions.dependency-injection/service-lookup/ServiceCacheKey';
 import { ServiceCallSite } from '@yohira/extensions.dependency-injection/service-lookup/ServiceCallSite';
+import { Err, Ok, Result } from 'ts-results-es';
+
+const tryGetValue = <K, V>(map: Map<K, V>, key: K): Result<V, undefined> => {
+	if (map.has(key)) {
+		return new Ok(map.get(key) as V);
+	} else {
+		return new Err(undefined);
+	}
+};
 
 // https://source.dot.net/#Microsoft.Extensions.DependencyInjection/ServiceLookup/CallSiteFactory.cs,b3200cd3834458b8,references
 export class CallSiteFactory implements IServiceProviderIsService {
+	private static readonly defaultSlot = 0;
+	private readonly callSiteCache = new Map<number, ServiceCallSite>();
+
 	constructor(descriptors: ICollection<ServiceDescriptor>) {}
+
+	private tryCreateExact = (
+		serviceType: Type,
+		callSiteChain: CallSiteChain,
+	): ServiceCallSite | undefined => {
+		// TODO
+
+		return undefined;
+	};
+
+	private tryCreateOpenGeneric = (
+		serviceType: Type,
+		callSiteChain: CallSiteChain,
+	): ServiceCallSite | undefined => {
+		// TODO
+
+		return undefined;
+	};
+
+	private tryCreateEnumerable = (
+		serviceType: Type,
+		callSiteChain: CallSiteChain,
+	): ServiceCallSite | undefined => {
+		// TODO
+
+		return undefined;
+	};
+
+	private createCallSite = (
+		serviceType: Type,
+		callSiteChain: CallSiteChain,
+	): ServiceCallSite | undefined => {
+		// REVIEW
+
+		// REVIEW: Lock.
+		const callSite =
+			this.tryCreateExact(serviceType, callSiteChain) ??
+			this.tryCreateOpenGeneric(serviceType, callSiteChain) ??
+			this.tryCreateEnumerable(serviceType, callSiteChain);
+
+		return callSite;
+	};
 
 	getCallSite = (
 		serviceType: Type,
 		callSiteChain: CallSiteChain,
 	): ServiceCallSite | undefined => {
-		// TODO
-		throw new Error('Method not implemented.');
+		const tryGetValueResult = tryGetValue(
+			this.callSiteCache,
+			new ServiceCacheKey(
+				serviceType,
+				CallSiteFactory.defaultSlot,
+			).getHashCode(),
+		);
+		return tryGetValueResult.ok
+			? tryGetValueResult.val
+			: this.createCallSite(serviceType, callSiteChain);
 	};
 
 	isService = (serviceType: Type): boolean => {
---
+++ packages/extensions.dependency-injection/src/service-lookup/ServiceCacheKey.ts
@@ -0,0 +1,30 @@
+import { Type } from '@yohira/base/Type';
+
+// https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript/7616484#7616484
+const getHashCode = (value: string): number => {
+	let hashCode = 0;
+	if (value.length === 0) {
+		return hashCode;
+	}
+	for (let i = 0; i < value.length; i++) {
+		const char = value.charCodeAt(i);
+		hashCode = (hashCode << 5) - hashCode + char;
+		hashCode = hashCode | 0;
+	}
+	return hashCode;
+};
+
+// https://source.dot.net/#Microsoft.Extensions.DependencyInjection/ServiceLookup/ServiceCacheKey.cs,5f18c3f8ffa0308b,references
+export class ServiceCacheKey {
+	constructor(readonly type: Type | undefined, readonly slot: number) {}
+
+	static readonly empty = new ServiceCacheKey(undefined, 0);
+
+	equals = (other: ServiceCacheKey): boolean => {
+		return this.type === other.type && this.slot === other.slot;
+	};
+
+	getHashCode = (): number => {
+		return ((this.type ? getHashCode(this.type) : 23) * 397) ^ this.slot;
+	};
+}
