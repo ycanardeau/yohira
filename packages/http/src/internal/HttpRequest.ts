57c2a1f33e988324c1ab80345ef3e7d85c7662cc
Aigamo <ycanardeau@gmail.com>
2023-01-20 12:54:30 +0000

    Implement HttpRequest.httpRequestFeature

--- packages/http/src/internal/HttpRequest.ts
+++ packages/http/src/internal/HttpRequest.ts
@@ -1,3 +1,5 @@
+import { Type } from '@yohira/base';
+import { FeatureReferences } from '@yohira/extensions.features';
 import {
 	IHttpContext,
 	IHttpRequest,
@@ -5,13 +7,42 @@ import {
 } from '@yohira/http.abstractions';
 import { IHttpRequestFeature } from '@yohira/http.features';
 
+// https://source.dot.net/#Microsoft.AspNetCore.Http/Internal/DefaultHttpRequest.cs,00ce2db34b5033bf,references
+class FeatureInterfaces {
+	request?: IHttpRequestFeature;
+}
+
 // https://source.dot.net/#Microsoft.AspNetCore.Http/Internal/DefaultHttpRequest.cs,7c96c43ce8999806,references
 export class HttpRequest implements IHttpRequest {
-	constructor(readonly httpContext: IHttpContext) {}
+	private static readonly nullRequestFeature = ():
+		| IHttpRequestFeature
+		| undefined => {
+		return undefined;
+	};
+
+	private features = new FeatureReferences<FeatureInterfaces>(
+		FeatureInterfaces,
+	);
+
+	constructor(readonly httpContext: IHttpContext) {
+		this.features.initialize(httpContext.features);
+	}
 
 	get httpRequestFeature(): IHttpRequestFeature {
-		// TODO
-		throw new Error('Method not implemented.');
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		return this.features.fetch(
+			Type.from('IHttpRequestFeature'),
+			{
+				get: () => {
+					return this.features.cache.request;
+				},
+				set: (value) => {
+					this.features.cache.request = value;
+				},
+			},
+			this.features.collection,
+			HttpRequest.nullRequestFeature,
+		)!;
 	}
 
 	get method(): string {
