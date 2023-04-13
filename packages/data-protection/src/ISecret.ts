ef7810c8c56a4966bf6aaf13dd51531c52cd5b99
Aigamo <ycanardeau@gmail.com>
2023-04-13 23:05:27 +0000

    Add unit tests for ManagedAuthenticatedEncryptor

--- packages/data-protection/src/ISecret.ts
+++ packages/data-protection/src/ISecret.ts
@@ -3,4 +3,5 @@ import { IDisposable } from '@yohira/base';
 // https://source.dot.net/#Microsoft.AspNetCore.DataProtection/ISecret.cs,2e17eebed5eb8518,references
 export interface ISecret extends IDisposable {
 	readonly length: number;
+	writeSecretIntoBuffer(buffer: Buffer): void;
 }
---
+++ packages/data-protection/src/cng/IBCryptGenRandom.ts
@@ -0,0 +1,4 @@
+// https://source.dot.net/#Microsoft.AspNetCore.DataProtection/Cng/IBCryptGenRandom.cs,9e7893097519395c,references
+export interface IBCryptGenRandom {
+	genRandomBCrypt(/* TODO */): void;
+}
