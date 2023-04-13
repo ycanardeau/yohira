269c3d20299cf30eaacac2295005f9e9cb5d6272
Aigamo <ycanardeau@gmail.com>
2023-04-13 00:44:52 +0000

    Add unit tests for CacheableKeyRing

---
+++ packages/data-protection/test/key-management/CacheableKeyRing.test.ts
@@ -0,0 +1,52 @@
+import { CacheableKeyRing, IKeyRing } from '@yohira/data-protection';
+import { expect, test } from 'vitest';
+
+// https://github.com/dotnet/aspnetcore/blob/2745e0b1e0b8bdfe428d8d115cae0d0f42bcea7b/src/DataProtection/DataProtection/test/KeyManagement/CacheableKeyRingTests.cs#L12
+test('IsValid_NullKeyRing_ReturnsFalse', () => {
+	expect(CacheableKeyRing.isValid(undefined, Date.now())).toBe(false);
+});
+
+// https://github.com/dotnet/aspnetcore/blob/2745e0b1e0b8bdfe428d8d115cae0d0f42bcea7b/src/DataProtection/DataProtection/test/KeyManagement/CacheableKeyRingTests.cs#L18
+test('IsValid_CancellationTokenTriggered_ReturnsFalse', () => {
+	const keyRing: IKeyRing = {} as IKeyRing;
+	const now = Date.now();
+	// TODO: const cts =
+	const cacheableKeyRing = new CacheableKeyRing(
+		// TODO: cts.token,
+		now + 1 * 60 * 60 * 1000,
+		keyRing,
+	);
+
+	expect(CacheableKeyRing.isValid(cacheableKeyRing, now)).toBe(true);
+	// TODO: cts.cancel();
+	// TODO: expect(CacheableKeyRing.isValid(cacheableKeyRing, now)).toBe(false);
+});
+
+// https://github.com/dotnet/aspnetcore/blob/2745e0b1e0b8bdfe428d8d115cae0d0f42bcea7b/src/DataProtection/DataProtection/test/KeyManagement/CacheableKeyRingTests.cs#L33
+test('IsValid_Expired_ReturnsFalse', () => {
+	const keyRing: IKeyRing = {} as IKeyRing;
+	const now = Date.now();
+	// TODO: const cts =
+	const cacheableKeyRing = new CacheableKeyRing(
+		// TODO: cts.token,
+		now + 1 * 60 * 60 * 1000,
+		keyRing,
+	);
+
+	expect(CacheableKeyRing.isValid(cacheableKeyRing, now)).toBe(true);
+	expect(
+		CacheableKeyRing.isValid(cacheableKeyRing, now + 1 * 60 * 60 * 1000),
+	).toBe(false);
+});
+
+// https://github.com/dotnet/aspnetcore/blob/2745e0b1e0b8bdfe428d8d115cae0d0f42bcea7b/src/DataProtection/DataProtection/test/KeyManagement/CacheableKeyRingTests.cs#L47
+test('KeyRing_Prop', () => {
+	const keyRing: IKeyRing = {} as IKeyRing;
+	const cacheableKeyRing = new CacheableKeyRing(
+		// TODO
+		Date.now(),
+		keyRing,
+	);
+
+	expect(cacheableKeyRing.keyRing === keyRing).toBe(true);
+});
