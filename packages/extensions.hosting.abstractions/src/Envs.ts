8b489f806aeb6601be8f1ba14f7ab7b6304b6198
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2024-02-23 15:16:02 +0900

    Add isStaging and isProduction to HostEnvEnvExtensions

--- packages/extensions.hosting.abstractions/src/Envs.ts
+++ packages/extensions.hosting.abstractions/src/Envs.ts
@@ -1,3 +1,7 @@
+// https://source.dot.net/#Microsoft.Extensions.Hosting.Abstractions/Environments.cs,edcfb5e2ed085493,references
+/**
+ * Commonly used environment names.
+ */
 export enum Envs {
 	Development = 'development',
 	Staging = 'staging',
--- packages/extensions.hosting.abstractions/src/HostEnvEnvExtensions.ts
+++ packages/extensions.hosting.abstractions/src/HostEnvEnvExtensions.ts
@@ -2,11 +2,42 @@ import { Envs } from './Envs';
 import { IHostEnv } from './IHostEnv';
 
 // https://source.dot.net/#Microsoft.Extensions.Hosting.Abstractions/HostEnvironmentEnvExtensions.cs,fea784c9bbb70c60,references
-export function isEnvironment(hostEnv: IHostEnv, envName: string): boolean {
+/**
+ * Compares the current host environment name against the specified value.
+ * @param hostEnv An instance of {@link IHostEnv}.
+ * @param envName Environment name to validate against.
+ * @returns True if the specified name is the same as the current environment, otherwise false.
+ */
+export function isEnv(hostEnv: IHostEnv, envName: string): boolean {
 	return hostEnv.envName.toLowerCase() === envName.toLowerCase();
 }
 
 // https://source.dot.net/#Microsoft.Extensions.Hosting.Abstractions/HostEnvironmentEnvExtensions.cs,7b43b7392a224997,references
+/**
+ * Checks if the current host environment name is {@link Envs.Development}.
+ * @param hostEnv An instance of {@link IHostEnv}.
+ * @returns True if the environment name is {@link Envs.Development}, otherwise false.
+ */
 export function isDevelopment(hostEnv: IHostEnv): boolean {
-	return isEnvironment(hostEnv, Envs.Development);
+	return isEnv(hostEnv, Envs.Development);
+}
+
+// https://source.dot.net/#Microsoft.Extensions.Hosting.Abstractions/HostEnvironmentEnvExtensions.cs,fea784c9bbb70c60,references
+/**
+ * Checks if the current host environment name is {@link Envs.Staging}.
+ * @param hostEnv An instance of {@link IHostEnv}.
+ * @returns True if the environment name is {@link Envs.Staging}, otherwise false.
+ */
+export function isStaging(hostEnv: IHostEnv): boolean {
+	return isEnv(hostEnv, Envs.Staging);
+}
+
+// https://source.dot.net/#Microsoft.Extensions.Hosting.Abstractions/HostEnvironmentEnvExtensions.cs,eea10e6a7e146ad2,references
+/**
+ * Checks if the current host environment name is {@link Envs.Production}.
+ * @param hostEnv An instance of {@link IHostEnv}.
+ * @returns True if the environment name is {@link Envs.Production}, otherwise false.
+ */
+export function isProduction(hostEnv: IHostEnv): boolean {
+	return isEnv(hostEnv, Envs.Production);
 }
