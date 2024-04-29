06d0a14e946415efdd4970d572c74dbc3711a0d5
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2024-04-29 23:28:27 +0900

    Fix AuthenticationTicket

--- packages/authentication.abstractions/src/AuthenticationTicket.ts
+++ packages/authentication.abstractions/src/AuthenticationTicket.ts
@@ -1,18 +1,19 @@
-// https://source.dot.net/#Microsoft.AspNetCore.Authentication.Abstractions/AuthenticationTicket.cs,f94b5ee0f273ee26,references
-import { AuthenticationProperties } from './AuthenticationProperties';
 import { ClaimsPrincipal } from '@yohira/base';
+
+import { AuthenticationProperties } from './AuthenticationProperties';
 
 // https://source.dot.net/#Microsoft.AspNetCore.Authentication.Abstractions/AuthenticationTicket.cs,f94b5ee0f273ee26,references
 /**
  * Contains user identity information as well as additional authentication state.
  */
 export class AuthenticationTicket {
+	readonly properties: AuthenticationProperties;
+
 	constructor(
 		readonly principal: ClaimsPrincipal,
-		readonly properties = new AuthenticationProperties(
-			undefined,
-			undefined,
-		),
+		properties = new AuthenticationProperties(undefined, undefined),
 		readonly authenticationScheme: string,
-	) {}
+	) {
+		this.properties = properties;
+	}
 }
