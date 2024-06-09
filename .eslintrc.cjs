330d6d6b389b959c1d76ff1964bcb269c6798c61
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2024-06-09 11:09:31 +0900

    Install eslint-plugin-boundaries

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,19 +5,37 @@ module.exports = {
 		sourceType: 'module',
 		tsconfigRootDir: __dirname,
 	},
-	plugins: ['@typescript-eslint/eslint-plugin'],
-	extends: ['plugin:@typescript-eslint/recommended'],
+	plugins: ['@typescript-eslint/eslint-plugin', 'boundaries'],
+	extends: [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:boundaries/recommended',
+	],
 	root: true,
 	env: {
 		node: true,
 		jest: true,
 	},
 	ignorePatterns: ['.eslintrc.cjs'],
+	settings: {
+		'import/resolver': {
+			typescript: {
+				alwaysTryTypes: true,
+			},
+		},
+		'boundaries/elements': [],
+	},
 	rules: {
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'error',
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/no-empty-function': 'off',
+		'boundaries/element-types': [
+			2,
+			{
+				default: 'disallow',
+				rules: [],
+			},
+		],
 	},
 };
